12e02be24a395d1a36c029db50d46330fc0186ae
David Scotton <dscotton@gmail.com>
2013-06-06 00:29:07 -0700

    Added some comments on CookMode.

--- web/lib/game_mode.dart
+++ web/lib/game_mode.dart
@@ -51,6 +51,9 @@ class TitleMode extends GameMode {
   }
 }
 
+/**
+ * This is the 2d overhead mode in which exploration takes place.
+ */
 class ExploreMode extends GameMode {
   AssetManager assetManager;
 
@@ -65,8 +68,30 @@ class ExploreMode extends GameMode {
   }
 }
 
+/**
+ * Boss battle mode where you time button presses to prepare a meal.
+ */
 class CookMode extends GameMode {
   AssetManager assetManager;
+
+  /*
+   * Graphically this mode needs several elements:
+   * - A background image (probably generated from tiles but should only be
+   *   rendered once).
+   * - An enemy sprite.  This will have some animations but no gameplay logic
+   *   except possibly charging the player when time runs out.
+   * - A player sprite.  This also should have some animations but they will
+   *   respond to game state (combo effectiveness) rather than directly to
+   *   player actions.
+   * - A rhythm status timer showing when buttons should be pressed.
+   * - Text boxes that pop up to indicate successfulness of combos.
+   *
+   * The data should just be an array (or array of arrays) of buttons that
+   * need to be pressed and timings before pressing them, along with potential
+   * score for each.  Big open question: how will these be generated,
+   * since the items are based on user inventory selection?  Do bosses
+   * have their own Rhythms, are the timings always constant, or what?
+   */
 
   CookMode(this.assetManager);
 
