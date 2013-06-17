85dd1cf5d9a219f09b787a72eb012c340391be1d
James Vogel <james@appliedmemetics.net>
2013-06-16 21:11:51 -0700

    removed some dodgy stuff.

--- web/lib/creature.dart
+++ web/lib/creature.dart
@@ -9,42 +9,11 @@ part of angler_game;
  * A creature is a game entity capable of moving independently.
  */
 abstract class Creature extends Sprite {
-  final int speed = 10;  // sample speed
+  final int speed = 5;  // sample speed
   Vector2 destination;
   Creature(int x, int y, image) : super(x, y, image);
   void SetDestCollisionDetect(Vector2 dest, List<Immobile> immobs) {
     destination = dest;
-    Vector2 delta = destination - loc;
-    // We're checking to see if any of the corner-to-corner paths intersect any of
-    // the boundry walls for each immobile object. This will fail for objects smaller
-    // than the player.
-    Vector2 l_t = new Vector2(rect.left.toDouble(), rect.top.toDouble());
-    Vector2 r_t = new Vector2(rect.right.toDouble(), rect.top.toDouble());
-    Vector2 l_b = new Vector2(rect.left.toDouble(), rect.bottom.toDouble());
-    Vector2 r_b = new Vector2(rect.right.toDouble(), rect.bottom.toDouble());
-    for (Immobile immob in immobs) {
-      // Check if the corner vector intersects any boundry of immob; if it does, fix destination along that axis.
-      if (loc.x <= destination.x &&
-          (immob.crossLeft(l_t, l_t + delta) || immob.crossLeft(r_t, r_t + delta) ||
-           immob.crossLeft(l_b, l_b + delta) || immob.crossLeft(r_b, r_b + delta))) {
-        destination.x = immob.rect.left - (rect.width / 2) - 1; 
-      }
-      if (loc.y <= destination.y &&
-          (immob.crossTop(l_t, l_t + delta) || immob.crossTop(r_t, r_t + delta) ||
-           immob.crossTop(l_b, l_b + delta) || immob.crossTop(r_b, r_b + delta))) {
-        destination.y = immob.rect.top - (rect.height / 2) - 1;
-      }
-      if (loc.x > destination.x &&
-          (immob.crossRight(l_t, l_t + delta) || immob.crossRight(r_t, r_t + delta) ||
-           immob.crossRight(l_b, l_b + delta) || immob.crossRight(r_b, r_b + delta))) {
-        destination.x = immob.rect.right + (rect.width / 2) + 1; 
-      }
-      if (loc.y > destination.y &&
-          (immob.crossBottom(l_t, l_t + delta) || immob.crossBottom(r_t, r_t + delta) ||
-           immob.crossBottom(l_b, l_b + delta) || immob.crossBottom(r_b, r_b + delta))) {
-        destination.y = immob.rect.bottom + (rect.height / 2) + 1;
-      }
-    }    
   }
   void MoveToward(Vector2 v, int distance) {
     Vector2 delta = v - loc;
@@ -72,8 +41,6 @@ abstract class Creature extends Sprite {
 
 // The player object
 class Player extends Creature {
-  static final int STEPS_PER_TICK = 10;
-
   List<Ingredient> inventory;
   List<List<Ingredient>> equipped;
 
--- web/lib/sprite.dart
+++ web/lib/sprite.dart
@@ -15,38 +15,4 @@ class Sprite {
   bool collidesWith(Sprite other) {
     return rect.collidesWith(other.rect);
   }
-  bool crossLeft(Vector2 start, Vector2 end) {
-    return crossBorder(new Vector2(rect.left.toDouble(), rect.top.toDouble()),
-        new Vector2(rect.left.toDouble(), rect.bottom.toDouble()), start, end);
-  }
-  bool crossTop(Vector2 start, Vector2 end) {
-    return crossBorder(new Vector2(rect.left.toDouble(), rect.top.toDouble()),
-        new Vector2(rect.right.toDouble(), rect.top.toDouble()), start, end);
-  }
-  bool crossRight(Vector2 start, Vector2 end) {
-    return crossBorder(new Vector2(rect.right.toDouble(), rect.top.toDouble()),
-        new Vector2(rect.right.toDouble(), rect.bottom.toDouble()), start, end);
-  }
-  bool crossBottom(Vector2 start, Vector2 end) {
-    return crossBorder(new Vector2(rect.left.toDouble(), rect.bottom.toDouble()),
-        new Vector2(rect.right.toDouble(), rect.bottom.toDouble()), start, end);
-  }
-  bool crossBorder(Vector2 my_start, Vector2 my_end, Vector2 test_start, Vector2 test_end) {
-    // I saw this math on StackOverflow therefore it cannot be wrong.
-    double x00 = test_start.x;
-    double x01 = test_end.x;
-    double x10 = my_start.x;
-    double x11 = my_end.x;
-    double y00 = test_start.y;
-    double y01 = test_end.y;
-    double y10 = my_start.y;
-    double y11 = my_end.y;
-    double det = x11 * y01 - x01 * y11;
-    if (det == 0) {
-      return false;
-    }
-    double s = (1 / det) * ((x00 - x10) * y01 - (y00 - y10) * x01);
-    double t = (1 / det) * -1 * (-1 * (x00 - x10) * y11 + (y00 - y10) * x11);
-    return 0 <= s && s <= 1 && 0 <= t && t <= 1;
-  }
 }
